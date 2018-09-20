4952409a9d1a5d6f81ea3a177019d558cd1f44db
mgardner3901 <31485789+mgardner3901@users.noreply.github.com>
2018-09-20 12:36:35 -0400

    Updated from Brython Server: 9/20/2018 12:36:33 PM

--- birthday.py
+++ birthday.py
@@ -32,11 +32,60 @@ Example Session
   Eric, you are a fall baby of the stone age.
 """
 
-name = input('What is your name? ')
-month = int(input('Hi '+name+ ', what is the name of the month were you born in? '))
-year = int(input('And what year were you born in, '+name+ '? '))
-day = int(input('And the day? '))
+a = input("Hello, what is your name? ")
+c = str("Hi " + a + ", what was the name of the month you were born in? ") 
+b = input(c)
+d = int(input("And what year were you born in, " + a + "? "))
+e = int(input("And the day? "))
 
-if month == int('October') and day = int('31'): 
-    print ('You were born on Halloween! ')
+months = ["" , "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ""]
+month = months[todaymonth]
+
+
+if b=="October" and e==31:
+    print("You were born on Halloween!")
+    
+    
+elif b==month and e==todaydate:
+    print("Happy birthday!")
+    
+    
+elif b in ["December", "January", "February"] and d>=2000:
+    print(str(a + ", you are a winter baby of the two thousands."))
+elif b in ["March", "April", "May"] and d>=2000: 
+    print(str(a + ", you are a spring baby of the two thousands."))
+elif b in ["June", "July", "August"] and d>=2000:
+    print(str(a +", you are a summer baby of the two thousands."))
+elif b in ["September", "October", "November"]  and d>=2000:
+    print(str(a + ", you are a fall baby of the two thousands."))
+    
+    
+elif b in ["December", "January", "February"] and d>=1990 and d<= 2000:
+    print(str(a + ", you are a winter baby of the nineties."))
+elif b in ["March", "April", "May"] and d>=1990 and d<= 2000:
+    print(str(a + ", you are a spring baby of the nineties."))
+elif b in ["June", "July", "August"] and d>=1990 and d<=2000:
+    print(str(a + ", you are a summer baby of the nineties."))
+elif b in ["September", "October", "November"]  and d>=1990 and d<=2000:
+    print(str(a + ", you are a fall baby of the nineties."))
+    
+    
+elif b in ["December", "January", "February"] and d>=1980 and d<= 1990:
+    print(str(a + ", you are a winter baby of the eighties."))
+elif b in ["March", "April", "May"] and d>=1980 and d<= 1990:
+    print(str(a + ", you are a spring baby of the eighties."))
+elif b in ["June", "July", "August"] and d>=1980 and d<=1990:
+    print(str(a + ", you are a summer baby of the eighties."))    
+elif b in ["September", "October", "November"]  and d>=1980 and d<=1990:
+    print(str(a + ", you are a fall baby of the eighties."))
+    
+    
+elif b in ["December", "January", "February"]  and d<=1980:
+    print(str(a + ", you are a winter baby of the Stone Age."))
+elif b in ["March", "April", "May"] and d<=1980: 
+    print(str(a + ", you are a spring baby of the Stone Age."))
+elif b in ["June", "July", "August"] and d<=1980:
+    print(str(a +", you are a summer baby of the Stone Age."))
+elif b in ["September", "October", "November"] and d<=1980:
+    print(str(a + ", you are a fall baby of the Stone Age."))
 
